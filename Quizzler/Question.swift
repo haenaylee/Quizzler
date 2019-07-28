6c71c21a4daaa724bcc1127144882c9c4cd44caf
Haena Lee <haenalee@Heejungs-iMac.local>
2019-07-28 09:33:55 -0400

    Add all files

---
+++ Quizzler/Question.swift
@@ -0,0 +1,20 @@
+//
+//  Question.swift
+//  Quizzler
+//
+//  Created by Haena Lee on 7/21/19.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Question {
+    let questionText : String
+    let answer : Bool
+    
+    init(text: String, correctAnswer: Bool){
+        questionText = text
+        answer = correctAnswer
+    }
+}
+
